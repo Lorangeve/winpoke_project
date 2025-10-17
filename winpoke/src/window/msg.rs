4309b7d3d1f008c680e357af7d1a64da3eb3f0f8
lorangeve <44472184+Lorangeve@users.noreply.github.com>
2025-10-17 14:49:47 +0800

    ✨ feat(input): 支持 SendInput、按键弹起与 Input 变体

--- winpoke/src/window/msg.rs
+++ winpoke/src/window/msg.rs
@@ -1,5 +1,11 @@
 use windows::Win32::Foundation::{HWND, LPARAM, WPARAM};
-use windows::Win32::UI::WindowsAndMessaging::{SendMessageW, WM_CHAR, WM_COMMAND, WM_KEYDOWN};
+use windows::Win32::UI::Input::KeyboardAndMouse::SendInput;
+use windows::Win32::UI::Input::KeyboardAndMouse::{
+    INPUT, INPUT_0, INPUT_KEYBOARD, KEYBD_EVENT_FLAGS, KEYBDINPUT, VIRTUAL_KEY,
+};
+use windows::Win32::UI::WindowsAndMessaging::{
+    SendMessageW, WM_CHAR, WM_COMMAND, WM_KEYDOWN, WM_KEYUP,
+};
 
 use crate::prelude::Result;
 
@@ -12,15 +18,26 @@ pub struct Message {
 #[derive(Debug)]
 pub enum WindowMessage {
     KeyDown(u32),
-    MouseMove(i32, i32),
+    KeyUp(u32),
     Char(char),
+    MouseMove(i32, i32),
     Command(u32),
+    Input(InputSequence),
 }
 
 impl Default for WindowMessage {
     fn default() -> Self {
         WindowMessage::KeyDown(0)
     }
+}
+
+pub type InputSequence = Vec<InputMessage>;
+
+#[derive(Debug)]
+pub enum InputMessage {
+    Mouse { x: i32, y: i32 },
+    Keyboard { key: u32, flag: Option<u32> },
+    Hardware,
 }
 
 pub(crate) fn send_message(
@@ -56,44 +73,120 @@ pub(crate) fn send_message_seq(hwnd: HWND, msg_seq: Vec<Message>) -> Result<()> 
             WindowMessage::KeyDown(virtual_key) => {
                 send_message(hwnd, WM_KEYDOWN, Some(virtual_key), None, message.count)?
             }
+            WindowMessage::KeyUp(virtual_key) => {
+                send_message(hwnd, WM_KEYUP, Some(virtual_key), None, message.count)?
+            }
             WindowMessage::MouseMove(_, _) => todo!(),
+            WindowMessage::Input(input_messages) => send_input_seq(input_messages)?,
         }
     }
 
     Ok(())
 }
 
+pub(crate) fn send_input_seq(input_seq: InputSequence) -> Result<()> {
+    let cbsize = std::mem::size_of::<INPUT>();
+
+    let mut inputs: Vec<INPUT> = Vec::new();
+
+    for input in input_seq {
+        match input {
+            InputMessage::Mouse { x, y } => todo!(),
+            InputMessage::Keyboard { key, flag } => {
+                inputs.push(INPUT {
+                    r#type: INPUT_KEYBOARD,
+                    Anonymous: INPUT_0 {
+                        ki: KEYBDINPUT {
+                            wVk: VIRTUAL_KEY(key as _),
+                            dwFlags: KEYBD_EVENT_FLAGS(flag.unwrap_or_default()),
+                            ..Default::default()
+                        },
+                    },
+                    ..Default::default()
+                });
+            }
+            InputMessage::Hardware => todo!(),
+        }
+    }
+
+    unsafe { SendInput(inputs.as_ref(), cbsize as _) };
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use windows::Win32::UI::Input::KeyboardAndMouse::{KEYEVENTF_KEYUP, VK_LEFT, VK_LWIN};
+
     use super::*;
     use crate::{error::Error, window::WindowInfo};
-
-    use windows::Win32::UI::Input::KeyboardAndMouse::VK_LEFT;
 
     #[test]
     fn test_send_message() -> Result<()> {
         let windows = WindowInfo::find_by_class_name("RegEdit_RegEdit") // "RegEdit_RegEdit"
             .expect("找不到指定窗口");
         let window = windows.into_iter().next().expect("没有窗口信息");
+        window.set_foreground_window()?;
 
         let tree_wnd = window
-            .get_child_windows()?
+            .find_child_windows()?
             .into_iter()
             .filter(|w| w.class_name == "SysTreeView32")
             .next()
             .ok_or(Error::WindowNotFound)?;
 
-        send_message_seq(
-            tree_wnd.hwnd,
-            dbg!(vec![
+        tree_wnd.show_window()?;
+        tree_wnd.set_focus()?;
+
+        tree_wnd
+            .send_message_seq(dbg!(vec![
                 // 循环发送左箭头，折叠到根节点
                 Message {
                     msg: WindowMessage::KeyDown(VK_LEFT.0 as _),
                     count: 5,
                 },
-            ]),
-        )
-        .expect("发送消息失败");
+                Message {
+                    msg: WindowMessage::KeyDown(VK_LWIN.0 as _),
+                    ..Default::default()
+                },
+                Message {
+                    msg: WindowMessage::KeyDown('D' as _),
+                    ..Default::default()
+                },
+                Message {
+                    msg: WindowMessage::KeyUp(VK_LWIN.0 as _),
+                    ..Default::default()
+                },
+                Message {
+                    msg: WindowMessage::KeyUp('D' as _),
+                    ..Default::default()
+                },
+            ]))
+            .expect("发送消息失败");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_send_input() -> Result<()> {
+        send_input_seq(vec![
+            InputMessage::Keyboard {
+                key: VK_LWIN.0 as _,
+                flag: None,
+            },
+            InputMessage::Keyboard {
+                key: 'D' as _,
+                flag: None,
+            },
+            InputMessage::Keyboard {
+                key: VK_LWIN.0 as _,
+                flag: Some(KEYEVENTF_KEYUP.0),
+            },
+            InputMessage::Keyboard {
+                key: 'D' as _,
+                flag: Some(KEYEVENTF_KEYUP.0),
+            },
+        ])?;
 
         Ok(())
     }
