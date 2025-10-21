4561ff51db60c3822ea95db8f94753bf184acde0
lorangeve <44472184+Lorangeve@users.noreply.github.com>
2025-10-21 14:25:14 +0800

    Add mouse input support to window message handling

--- winpoke/src/window/msg.rs
+++ winpoke/src/window/msg.rs
@@ -1,12 +1,15 @@
 pub mod keyboard;
+pub mod mouse;
 
-use windows::Win32::Foundation::{HWND, LPARAM, WPARAM};
+use windows::Win32::Foundation::{HWND, LPARAM, POINT, WPARAM};
 use windows::Win32::UI::Input::KeyboardAndMouse::SendInput;
 use windows::Win32::UI::Input::KeyboardAndMouse::{
-    INPUT, INPUT_0, INPUT_KEYBOARD, KEYBD_EVENT_FLAGS, KEYBDINPUT, VIRTUAL_KEY,
+    INPUT, INPUT_0, INPUT_KEYBOARD, INPUT_MOUSE, KEYBD_EVENT_FLAGS, KEYBDINPUT, MOUSEEVENTF_MOVE,
+    MOUSEINPUT, VIRTUAL_KEY,
 };
 use windows::Win32::UI::WindowsAndMessaging::{
-    SendMessageW, WM_CHAR, WM_COMMAND, WM_KEYDOWN, WM_KEYUP,
+    GetCursorPos, SendMessageW, WM_CHAR, WM_COMMAND, WM_KEYDOWN, WM_KEYUP, WM_LBUTTONDOWN,
+    WM_LBUTTONUP, WM_MOUSEMOVE,
 };
 
 use crate::prelude::Result;
@@ -19,11 +22,23 @@ pub struct Message {
 
 #[derive(Debug)]
 pub enum WindowMessage {
+    /// 按下键盘按键，参数为虚拟键码
     KeyDown(u32),
+    /// 松开键盘按键，参数为虚拟键码
     KeyUp(u32),
+    /// 输入字符，参数为字符
     Char(char),
-    MouseMove(i32, i32),
+    /// 移动鼠标到指定坐标
+    MouseMoveTo(i32, i32),
+    /// 相对于当前位置移动鼠标
+    MouseMoveBy(i32, i32),
+    /// 单击鼠标左键，参数为坐标
+    MouseClick(u32, u32),
+    /// 双击鼠标左键，参数为坐标
+    MouseDoubleClick(u32, u32),
+    /// 发送命令消息，参数为命令ID
     Command(u32),
+    /// 发送输入序列，参数为输入消息序列
     Input(InputSequence),
 }
 
@@ -39,7 +54,6 @@ pub type InputSequence = Vec<InputMessage>;
 pub enum InputMessage {
     Mouse { x: i32, y: i32 },
     Keyboard { key: u32, flag: Option<u32> },
-    Hardware,
 }
 
 pub(crate) fn send_message(
@@ -78,7 +92,32 @@ pub(crate) fn send_message_seq(hwnd: HWND, msg_seq: Vec<Message>) -> Result<()> 
             WindowMessage::KeyUp(virtual_key) => {
                 send_message(hwnd, WM_KEYUP, Some(virtual_key), None, message.count)?
             }
-            WindowMessage::MouseMove(_, _) => todo!(),
+            WindowMessage::MouseMoveTo(x, y) => {
+                let lparam = ((y as u32) << 16) | (x as u32);
+                send_message(hwnd, WM_MOUSEMOVE, None, Some(lparam), message.count)?;
+            }
+            WindowMessage::MouseMoveBy(x, y) => {
+                // 获取当前鼠标位置
+                let mut point = POINT::default();
+                unsafe { GetCursorPos(&mut point) }?;
+
+                let new_x = point.x + x;
+                let new_y = point.y + y;
+                let lparam = ((new_y as u32) << 16) | (new_x as u32);
+                send_message(hwnd, WM_MOUSEMOVE, None, Some(lparam), message.count)?;
+            }
+            WindowMessage::MouseClick(x, y) => {
+                let lparam = ((y as u32) << 16) | (x as u32);
+                send_message(hwnd, WM_LBUTTONDOWN, None, Some(lparam), message.count)?;
+                send_message(hwnd, WM_LBUTTONUP, None, Some(lparam), message.count)?;
+            }
+            WindowMessage::MouseDoubleClick(x, y) => {
+                let lparam = ((y as u32) << 16) | (x as u32);
+                send_message(hwnd, WM_LBUTTONDOWN, None, Some(lparam), message.count)?;
+                send_message(hwnd, WM_LBUTTONUP, None, Some(lparam), message.count)?;
+                send_message(hwnd, WM_LBUTTONDOWN, None, Some(lparam), message.count)?;
+                send_message(hwnd, WM_LBUTTONUP, None, Some(lparam), message.count)?;
+            }
             WindowMessage::Input(input_messages) => send_input_seq(input_messages)?,
         }
     }
@@ -93,7 +132,21 @@ pub(crate) fn send_input_seq(input_seq: InputSequence) -> Result<()> {
 
     for input in input_seq {
         match input {
-            InputMessage::Mouse { x, y } => todo!(),
+            InputMessage::Mouse { x, y } => {
+                inputs.push(INPUT {
+                    r#type: INPUT_MOUSE,
+                    Anonymous: INPUT_0 {
+                        mi: MOUSEINPUT {
+                            dx: x,
+                            dy: y,
+                            mouseData: 0,
+                            dwFlags: MOUSEEVENTF_MOVE,
+                            time: 0,
+                            dwExtraInfo: 0,
+                        },
+                    },
+                });
+            }
             InputMessage::Keyboard { key, flag } => {
                 inputs.push(INPUT {
                     r#type: INPUT_KEYBOARD,
@@ -106,7 +159,6 @@ pub(crate) fn send_input_seq(input_seq: InputSequence) -> Result<()> {
                     },
                 });
             }
-            InputMessage::Hardware => todo!(),
         }
     }
 
@@ -117,7 +169,7 @@ pub(crate) fn send_input_seq(input_seq: InputSequence) -> Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use windows::Win32::UI::Input::KeyboardAndMouse::{KEYEVENTF_KEYUP, VK_LEFT, VK_LWIN};
+    use windows::Win32::UI::Input::KeyboardAndMouse::KEYEVENTF_KEYUP;
 
     use super::*;
     use crate::{error::Error, prelude::Keyboard, window::WindowInfo};
@@ -188,6 +240,26 @@ mod tests {
                 flag: Some(KEYEVENTF_KEYUP.0),
             },
         ])?;
+
+        Ok(())
+    }
+
+    #[test]
+    /// 移动鼠标到屏幕中心
+    fn test_send_mouse_input() -> Result<()> {
+        use windows::Win32::UI::WindowsAndMessaging::GetSystemMetrics;
+        use windows::Win32::UI::WindowsAndMessaging::{SM_CXSCREEN, SM_CYSCREEN};
+
+        let screen_width = unsafe { GetSystemMetrics(SM_CXSCREEN) };
+        let screen_height = unsafe { GetSystemMetrics(SM_CYSCREEN) };
+
+        let center_x = (screen_width / 2) * 65535 / screen_width;
+        let center_y = (screen_height / 2) * 65535 / screen_height;
+
+        send_input_seq(vec![InputMessage::Mouse {
+            x: center_x,
+            y: center_y,
+        }])?;
 
         Ok(())
     }
