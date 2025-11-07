3abed2cda72fbb036363ae01d8343085413d0dc0
lorangeve <44472184+Lorangeve@users.noreply.github.com>
2025-11-07 18:28:39 +0800

    Implement mouse movement and click example

--- winpoke/examples/move_mouse.rs
+++ winpoke/examples/move_mouse.rs
@@ -1,3 +1,15 @@
+use winpoke::window::msg::input::{Input, MouseMessage};
+
 fn main() {
-    todo!()
+    let (x, y) = winpoke::monitor::MonitorInfo::primary_monitor()
+        .unwrap()
+        .map_to_virtual_screen(0, 0);
+    
+    Input::send_seq(&vec![
+        Input::Mouse(MouseMessage::MoveTo(x, y)),
+        Input::Mouse(MouseMessage::RightClick),
+        Input::Mouse(MouseMessage::Move(10, 10)),
+        Input::Mouse(MouseMessage::LeftClick),
+    ])
+    .unwrap();
 }
